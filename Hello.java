f80d6eae5efd0e2f1731f8c7b7c8bde9863bc7e4
JooHo <seng4169@naver.com>
2017-01-09 10:45:33 +0900

    modified java file

--- Hello.java
+++ Hello.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("hello world");
-		System.out.println("Tell Your world");
-		System.out.println("Tell his world");
-		System.out.println("Tell her world");
+		int sqaurLine,result;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("부피를 구할 정사각형에 밑변의 값을 입력해주세요:");
+		sqaurLine = scan.nextInt();
+		result = sqaurLine*sqaurLine;
+		System.out.println("부피는"+result+"입니다.");
 
 	}}
